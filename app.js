5f05feadc01fae354cc3ef993d699abc4a4b9e7f
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2025-04-14 19:33:05 +0530

    API 6.7 | Refactoring routes: 2

--- app.js
+++ app.js
@@ -88,11 +88,12 @@ const deleteTour = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(port, () => {
   console.log(`App is listening on http://localhost:${port}`);
