3f8773cf4499e52a2780ecc4e9f4b84b0668c4f0
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2025-05-13 11:18:43 +0530

    API 11.7 | add merged parameters for nested route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,20 @@ const express = require('express');
 const router = express.Router();
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
+
+// GET /tour/2345ad4a/reviews
+// GET /tour/2345ad4a/reviews/98204gs62
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview,
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -24,18 +37,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour,
-  );
-
-// POST /tour/2345ad4a/reviews
-// GET /tour/2345ad4a/reviews
-// GET /tour/2345ad4a/reviews/98204gs62
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
   );
 
 module.exports = router;
