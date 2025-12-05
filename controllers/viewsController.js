c2c1a9ed7af5e875853fd20c4c1543b066de14f1
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2025-12-05 21:26:14 +0530

    UI 1.6: Built the tour overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,19 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All Tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  try {
+    const tours = await Tour.find();
+    res.status(200).render('overview', { title: 'All Tours', tours });
+  } catch (err) {
+    next(err);
+  }
+
+  // 2) Build template
+
+  // 3) Render template using tour data from step 1
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forest Hiker' });
