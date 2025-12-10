ec66bf438ebe743886af9635fee77fa5e1660fe5
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2025-12-11 01:30:44 +0530

    UI 1.9: Log users from API: 1

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -69,6 +70,8 @@ app.use('/api', limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+app.use(cookieParser());
+
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
@@ -96,6 +99,7 @@ app.use(express.static(`${__dirname}/public/`));
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
---
+++ public/js/login.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+
+console.log('login.js loaded');
+
+console.log('axios in login.js:', typeof axios);
+
+const login = async (email, password) => {
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log(res);
+  } catch (err) {
+    console.log('ERROR FULL OBJECT:', err);
+
+    console.log(err.response.data);
+  }
+};
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
