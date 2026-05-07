5207d130a3482f823e16d3b038016b1ec7ef1157
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2026-05-07 14:11:29 +0530

    UI 1.26: Build password reset email

--- utils/email.js
+++ utils/email.js
@@ -63,4 +63,11 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Voya Family!');
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)',
+    );
+  }
 };
