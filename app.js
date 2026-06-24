3bcf2f03680e702bea63ef218837e9900e36ef77
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2026-06-25 00:44:11 +0530

    Pre-deploy 0.2: Add cors for simple and non simple requests

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -24,6 +25,11 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'base'); // default layout file
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors());
+
+// for non simple requests: PUT, PATCH, DELETE
+app.options('*', cors());
 
 // 1) GLOBAL MIDDLEWARES
 
