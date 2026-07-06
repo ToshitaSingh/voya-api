fa9added1215a8a12baab58890525ed31f0a55cc
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2026-07-07 00:23:26 +0530

    Pre-deploy 0.5: Configure Parcel production build

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ app.use(
 // Serving static files
 app.use(express.static(`${__dirname}/public/`));
 // app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/dist', express.static(`${__dirname}/dist`));
 
 app.use(compression());
 
